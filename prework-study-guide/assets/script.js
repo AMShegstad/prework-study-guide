ad01128cac282de3d3151af548c3ae854981733f
Alexander Shegstad <aShegstad@my365.Bellevue.edu>
2024-10-07 23:14:21 -0500

    Adding javaScript to application

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,19 @@
+const topics = ['HTML', 'CSS', 'Git', 'JavaSCript'];
+
+for(let x = 0; x < topics.length; x++) {
+    console.log("Let's study " + topics[x]);
+}
+
+/*
+if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+    console.log("Let's Study CSS!");
+} else if (topic === 'Git') {
+    console.log("Let's stidy Git!");
+} else if (topic === 'JavaScript') {
+    console.log("Let's study JavaSCript!");
+} else {
+    console.log('Please try again!');
+}
+*/
